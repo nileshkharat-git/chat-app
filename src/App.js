29edb698f0dd77d3ebb7e446e2324757b6ce5cac
nileshkharat-git <nileshk262000@gmail.com>
2024-08-08 10:03:13 +0530

    Add Browser router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// style
 import './App.css';
+
+// components
 import Header from './components/Header';
 import ContactList from './components/ContactList';
+
+// pages
+import Chat from './pages/Chat';
+
 function App() {
+  const { chat, setChat} = useState({});
+  
   return (
     <>
       <Header />
-      <ContactList />
+      <BrowserRouter>
+        <Routes>  
+          <Route path='/' element={<ContactList />} />
+          <Route path='/chat' element={<Chat />} />
+        </Routes>        
+      </BrowserRouter>
     </>
   );
 }
