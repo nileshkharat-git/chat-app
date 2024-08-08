1801bd5a1e08cb9354a9456cb61cacaf2a99a389
nileshkharat-git <nileshk262000@gmail.com>
2024-08-08 15:47:07 +0530

    Data updated

--- src/util/Data.js
+++ src/util/Data.js
@@ -5,25 +5,14 @@ const users = [
         id: 1,
         name: "Nilesh",
         status: "online",
-        profilePic:profile1
+        profilePic:profile1,
+
     },
-    // {
-    //     id: 2,
-    //     name: "Mrunmayee",
-    //     status: "offline",
-    //     profilePic:""
-    // },
     {
         id: 3,
         name: "Tanmay",
         status: "Online",
         profilePic:profile2
     },
-    // {
-    //     id: 4,
-    //     name: "Krishi",
-    //     status: "offline",
-    //     profilePic:""
-    // }
 ]
 export default users
