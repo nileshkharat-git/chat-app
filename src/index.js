fbc4de1342c8eff073360e39bb0c9145d4059908
nileshkharat-git <nileshk262000@gmail.com>
2024-08-08 10:05:47 +0530

    import usercontext and wrap App component in UserContext provider.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// context
+import UserContext from './Context';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserContext.Provider value={{ username: 'Nilesh' }}>
+      <App />
+    </UserContext.Provider>
   </React.StrictMode>
 );
 
