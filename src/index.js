25de21caf3f950a4015ff580896b38b99988ec7e
nileshkharat-git <nileshk262000@gmail.com>
2024-08-17 09:23:51 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-// context
-import UserContext from './Context';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <UserContext.Provider value={{ username: 'Nilesh' }}>
-      <App />
-    </UserContext.Provider>
+    <App />
   </React.StrictMode>
 );
-
