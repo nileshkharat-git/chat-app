424bc446310845ed713a33f421660b4ca1ff1593
nileshkharat-git <nileshk262000@gmail.com>
2024-08-17 09:27:09 +0530

    create handleSend to send message to the server.

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,90 +1,97 @@
-import { useParams } from 'react-router-dom';
-import { BsAlignBottom, BsFillSendFill } from "react-icons/bs";
-import styled from 'styled-components';
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { BsFillSendFill } from "react-icons/bs";
+import styled from "styled-components";
+import socket from "../socket";
 
-import bg  from '../assets/chatbg.jpg';
-import { darkTheme } from '../util/Theme';
+import bg from "../assets/chatbg.jpg";
+import { darkTheme } from "../util/Theme";
 
 // components
-import Message from "../components/Message"
+import Message from "../components/Message";
+// context
+import { MessageContext } from "../Context";
 
 const Conatiner = styled.div`
-    width: 100%;
-    height: 100vh;
-    background-image: url(${bg});
-    background-size: cover; 
+  width: 100%;
+  height: 100vh;
+  background-image: url(${bg});
+  background-size: contain;
 `;
 
 const Input = styled.input`
   width: 75%;
   height: 5vh;
-  border:none;
-  border-radius:30px;
-  outline:none;
+  border: none;
+  border-radius: 30px;
+  outline: none;
   padding-left: 10px;
 `;
 
 const sendBtn = {
-  fontSize:"25px",
-  bottom:"20px",
-  margin:"auto",
+  fontSize: "25px",
+  bottom: "20px",
+  margin: "auto",
   right: "10px",
-  rotate:"45deg",
-  color:`${darkTheme.text_primary}`,
-
-
-}
+  rotate: "45deg",
+  color: `${darkTheme.text_primary}`,
+};
 const SendBtnWrapper = styled.div`
   width: 50px;
   height: 50px;
   display: flex;
   align-items: center;
-  background-color:${darkTheme.bg_primary};
-  border-radius:50%;
-  padding-right:2px;
+  background-color: ${darkTheme.bg_primary};
+  border-radius: 50%;
+  padding-right: 2px;
 `;
 
 const BottomPanel = styled.div`
-  display:flex;
+  display: flex;
   width: 100%;
-  justify-content:space-around;
-  align-items:center;
-  position:absolute;
-  bottom:10px;
-
+  justify-content: space-around;
+  align-items: center;
+  position: absolute;
+  bottom: 10px;
 `;
 
-const messages = [
-  {
-    text:"hii",
-    sender:"tanamy"
-  },
-  {
-    text:"what's going on",
-    sender:"me"
-  },
-  {
-    text:"nothing",
-    sender:"tanamy"
-  },
-]
 const Chat = () => {
-  const {id} = useParams()
-  // const currentuser = users.filter(user => user.id === id)
-  
+  const { userid } = useParams();
+  const [message, setMessage] = useContext(MessageContext);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    socket.on("new-message", (receiveData) => {
+      setMessage([...message, receiveData]);
+    });
+  }, [socket]);
+
+  const handleSend = () => {
+    let prepareData = {
+      text: text,
+      sender: userid,
+    };
+    socket.emit("send-message", prepareData);
+    setText("");
+  };
+
   return (
     <Conatiner>
-      {messages.map((message, id)=>(
-        <Message message={message}/>
+      {message.map((message, id) => (
+        <Message message={message} key={id} />
       ))}
-    <BottomPanel>
-      <Input></Input>
-      <SendBtnWrapper>
-        <BsFillSendFill style={sendBtn} />    
-      </SendBtnWrapper>
-    </BottomPanel>
+      <BottomPanel>
+        <Input
+          onInput={(e) => setText(e.target.value)}
+          value={text}
+          placeholder={`message from ${userid}`}
+        ></Input>
+        <SendBtnWrapper onClick={() => handleSend()}>
+          <BsFillSendFill style={sendBtn} />
+        </SendBtnWrapper>
+      </BottomPanel>
     </Conatiner>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
