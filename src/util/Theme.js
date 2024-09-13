de3d963ac2cf1afa5c3687a3acfad1e664e30968
nileshkharat-git <nileshk262000@gmail.com>
2024-09-13 17:33:26 +0530

    Change Hex code for colors

--- src/util/Theme.js
+++ src/util/Theme.js
@@ -1,6 +1,9 @@
 export const darkTheme = {
-    bg_primary:"#1995AD",
-    bg_secondary:"#A1D6E2",
+    bg_primary:"#00ADB5",
+    bg_secondary:"#F1F7B5",
     bg_regular:"#F1F1F2",
-    text_primary:"#FFFFFF",
+    text_primary:"#222831",
+    text_secondary:"#EEEEEE",
+    shadow:"#EEEEEE",
+    font_family:"arial,sans-serif",
 };
