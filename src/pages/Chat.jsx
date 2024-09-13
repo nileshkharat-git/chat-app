acfbdf7bfcb9c45e9a7e1da92b39a9df6dfd2f2a
nileshkharat-git <nileshk262000@gmail.com>
2024-09-13 17:30:18 +0530

    Create InfoBox component

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,6 +1,6 @@
-import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { BsFillSendFill } from "react-icons/bs";
+import {  useContext,useEffect,  useState } from "react";
+import { useParams, useNavigate} from "react-router-dom";
+import { BsFillSendFill, BsBoxArrowLeft } from "react-icons/bs";
 import styled from "styled-components";
 import socket from "../socket";
 
@@ -14,7 +14,7 @@ import { MessageContext } from "../Context";
 
 const Conatiner = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 92vh;
   background-image: url(${bg});
   background-size: contain;
 `;
@@ -34,7 +34,7 @@ const sendBtn = {
   margin: "auto",
   right: "10px",
   rotate: "45deg",
-  color: `${darkTheme.text_primary}`,
+  color: "white",
 };
 const SendBtnWrapper = styled.div`
   width: 50px;
@@ -46,6 +46,14 @@ const SendBtnWrapper = styled.div`
   padding-right: 2px;
 `;
 
+const NoMessage = styled.h1`
+    position: relative;
+    top: 10vh;
+    text-align: center;
+    color: ${darkTheme.text_primary};
+    font-family: ${darkTheme.font_family};
+    background-color: ${darkTheme.bg_secondary};
+`;
 const BottomPanel = styled.div`
   display: flex;
   width: 100%;
@@ -55,36 +63,70 @@ const BottomPanel = styled.div`
   bottom: 10px;
 `;
 
-const Chat = () => {
-  const { userid } = useParams();
-  const [message, setMessage] = useContext(MessageContext);
+const InfoBox = styled.div`
+  width: 100%;
+  height: 5vh;
+  display: flex;
+  background-color: ${darkTheme.bg_primary};
+  justify-content: space-between;
+`;
+
+const leaveIcon = {
+  cursor: "pointer",
+  color: "white",
+  fontSize: "1.5rem",
+  marginRight: "10px",
+};
+const Chat = ({setMessage}) => {
+  const {roomId} = useParams()
+  const messages = useContext(MessageContext);
+  
+  const [recentChats, setRecentChats] = useState([]);
   const [text, setText] = useState("");
+  const navigate = useNavigate();
+
+  const triggerLeaveRoom = () => {
+    socket.emit("leaveRoom",sessionStorage.getItem("room-name"));
+    sessionStorage.removeItem("room-name");
+    setMessage([]);
+    return navigate("/room-list");
+  };
 
   useEffect(() => {
-    socket.on("new-message", (receiveData) => {
-      setMessage([...message, receiveData]);
-    });
-  }, [socket]);
+    fetch(`http://localhost:8000/api/messages?roomId=${roomId}`)
+    .then(res => res.json())
+    .then(res => {
+      setRecentChats(res)      
+    })
+  }, [roomId])
 
   const handleSend = () => {
     let prepareData = {
       text: text,
-      sender: userid,
+      userId: sessionStorage.getItem("userID"),
+      roomId: roomId,
     };
-    socket.emit("send-message", prepareData);
+    socket.emit("send-message",sessionStorage.getItem("room-name"), prepareData);
     setText("");
   };
 
   return (
     <Conatiner>
-      {message.map((message, id) => (
+      <InfoBox>
+        <div>
+          <span style={{ color: "white" , fontSize:"1.5rem", fontFamily:"arial", marginLeft:"18px"}}>{sessionStorage.getItem("room-name")}</span>
+        </div>
+        <BsBoxArrowLeft style={leaveIcon} onClick={triggerLeaveRoom}/>
+      </InfoBox>
+      {[...messages,...recentChats].length > 0 ?[...recentChats, ...messages].map((message, id) => (
         <Message message={message} key={id} />
-      ))}
+      )):""}
+      
       <BottomPanel>
         <Input
           onInput={(e) => setText(e.target.value)}
           value={text}
-          placeholder={`message from ${userid}`}
+          placeholder={`send message`}
         ></Input>
         <SendBtnWrapper onClick={() => handleSend()}>
           <BsFillSendFill style={sendBtn} />
