1571ddc9c8b369d371d7265f5dc1a16269ceb148
nileshkharat-git <nileshk262000@gmail.com>
2024-09-13 17:35:57 +0530

    add useEffect hook for listening new messages from server

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// style
-import "./App.css";
-
+import socket from "./socket";
 // components
 import Header from "./components/Header";
-import ContactList from "./components/ContactList";
 
 // pages
-import Chat from "./pages/Chat";
-import Login from "./pages/Login";
+import { Chat, CreateRoom, Login } from "./pages";
 
+// context
 import { MessageContext } from "./Context";
+import RoomList from "./components/RoomList";
 
 function App() {
   const [message, setMessage] = useState([]);
+  useEffect(() => {
+    socket.on("new-message", (receivedMessage) => {
+      setMessage((prevMessages) => [...prevMessages, receivedMessage]);
+    });
+  }, [socket]);
 
   return (
     <>
-      <Header />
-      <MessageContext.Provider value={[message, setMessage]}>
-        <BrowserRouter>
+      <BrowserRouter>
+        <MessageContext.Provider value={message}>
+        <Header />
           <Routes>
-            <Route path="/" element={<ContactList />} />
-            <Route path="/chat/:userid" element={<Chat />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/chat/:roomId" element={<Chat setMessage={setMessage} />} />
+            <Route path="/create-room" element={<CreateRoom />} />
+            <Route path="/room-list" element={<RoomList />} />
           </Routes>
-        </BrowserRouter>
-      </MessageContext.Provider>
+        </MessageContext.Provider>
+      </BrowserRouter>
     </>
   );
 }
